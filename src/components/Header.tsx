4d34e03a1c0538be4b63d5d47b2159ecfb81e437
Andres Rodriguez Schiller <andyroschy@hotmail.com>
2025-01-16 14:48:48 -0300

    add placeholder components for the main page

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+
+  return <header>
+    <div>
+      <div>LOGO</div>
+      <div>Add moives</div>
+    </div>
+    <div>
+      <div>burger <menu></menu></div>
+      <div>notifications</div>
+      <div>login button</div>
+    </div>
+  </header>
+}
