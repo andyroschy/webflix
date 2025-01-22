4e2e86422360e356e8fc4e87ebcbdb6847bb0529
Andres Rodriguez Schiller <andyroschy@hotmail.com>
2025-01-22 00:56:54 -0300

    add hover state

--- src/components/MoviesSidebar.tsx
+++ src/components/MoviesSidebar.tsx
@@ -23,19 +23,30 @@ export function MoviesSideBar({ movies }: Props) {
 function MoviePreview({ movie }: { movie: Movie }) {
   return (
     <li
-      className="flex flex-col justify-end items-center pb-4 w-60 h-36 mb-8 text-white rounded-sm cursor-pointer"
+      className="transition-opacity relative left-0 top-0 cursor-pointer group"
       style={{
         backgroundImage: `url(${buildImgUrl(movie.image_url, "preview")}`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "100% 100%",
       }}
     >
-      <button className="w-10 h-10 mb-4 border-1 border-white border-solid rounded-[100px] bg-[#24242480]">
-        <IconPlay color="white" className="align-middle inline-block " />
-      </button>
-      <span className="text-center inline-block w-full text-base tracking-[4px] drop-shadow-titlesm">
-        {movie.name}
-      </span>
+      <div className="w-60 h-36 mb-8"></div>
+      <div className="absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-60 h-36 text-white rounded-sm group-hover:opacity-0 opacity-100 ">
+        <button className="w-10 h-10 mb-4 border-1 border-white border-solid rounded-[100px] bg-[#24242480]">
+          <IconPlay color="white" className="align-middle inline-block " />
+        </button>
+        <span className="text-center inline-block w-full text-base tracking-[4px] drop-shadow-titlesm">
+          {movie.name}
+        </span>
+      </div>
+      <div className="absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-60 h-36 text-white rounded-sm cursor-pointer opacity-0 group-hover:opacity-100">
+        <button className="w-10 h-10 mb-4 border-1 border-white border-solid rounded-[100px] bg-[#24242480]">
+          <IconPlay color="white" className="align-middle inline-block " />
+        </button>
+        <span className="text-center inline-block w-full text-base tracking-[4px] drop-shadow-titlesm">
+          {movie.name} hover
+        </span>
+      </div>
     </li>
   );
 }
