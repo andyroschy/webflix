2490495cf577c9fd0beb0e0ea7096e536f80ee27
Andres Rodriguez Schiller <andyroschy@hotmail.com>
2025-01-22 03:07:04 -0300

    add correct sizing for movie preview

--- src/components/MoviesSidebar.tsx
+++ src/components/MoviesSidebar.tsx
@@ -33,7 +33,7 @@ interface PreviewProps {
 function MoviePreview({ movie }: PreviewProps) {
   return (
     <li
-      className=" relative left-0 top-0 rounded-sm w-60 h-36 mb-8 cursor-pointer group/container"
+      className=" relative left-0 top-0 rounded-mdsm w-55 h-36 mb-8 cursor-pointer group/container"
       style={{
         backgroundImage: `url(${buildImgUrl(movie.image_url, "preview")}`,
         backgroundRepeat: "no-repeat",
@@ -48,7 +48,7 @@ function MoviePreview({ movie }: PreviewProps) {
 
 function DefaultState({ movie }: Pick<PreviewProps, "movie">) {
   return (
-    <div className="transition-opacity absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-60 h-36 text-white rounded-sm group-hover/container:opacity-0 opacity-100 ">
+    <div className="transition-opacity absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-full h-full text-white group-hover/container:opacity-0 opacity-100 ">
       <button className="w-10 h-10 mb-4 border-1 border-white border-solid rounded-[100px] bg-[#24242480]">
         <IconPlay color="white" className="align-middle inline-block " />
       </button>
@@ -61,9 +61,9 @@ function DefaultState({ movie }: Pick<PreviewProps, "movie">) {
 
 function HoverState({ movie }: Pick<PreviewProps, "movie">) {
   return (
-    <div className="transition-opacity absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-60 h-36 text-white rounded-sm cursor-pointer opacity-0 group-hover/container:opacity-100 p-6 bg-[#24242478]">
-      <div className="w-full flex flex-row justify-start">
-        <button className="group/icon w-6 h-6 min-w-6 mb-4 border-1 border-white hover:border-black border-solid rounded-[100px] bg-[#24242480] hover:bg-teal-400">
+    <div className="transition-opacity absolute left-0 top-0 flex flex-col justify-end items-center pb-4 w-full h-full text-white  cursor-pointer opacity-0 group-hover/container:opacity-100 p-6 bg-[#24242478]">
+      <div className="w-full flex flex-row justify-start mb-4">
+        <button className="group/icon w-6 h-6 min-w-6 border-1 border-white hover:border-black border-solid rounded-[100px] bg-[#24242480] hover:bg-teal-400">
           <IconPlay
             className="align-middle inline-block"
             width={"12"}
