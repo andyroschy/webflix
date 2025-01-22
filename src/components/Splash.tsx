553d047ad966ca1287554975fdcb05805ce9be45
Andres Rodriguez Schiller <andyroschy@hotmail.com>
2025-01-21 22:17:33 -0300

    approximate spalsh

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,12 +1,17 @@
 import { Movie } from "@prisma/client";
+import Button from "./button";
 
 export function Splash({ featured }: { featured: Movie }) {
   return (
     <div className="flex flex-col justify-end mb-8">
-      Orignal de LITEFLIX
-      <h1 className="text-title text-teal-300">{featured.name}</h1>
+      <span className="text-xl/5 tracking-[3px] mb-4 drop-shadow-titlesm">
+        <span className="font-normal ">Orignal de </span>
+        <span className="font-bold">LITEFLIX</span>
+      </span>
+      {/* negative indent to compensenate "padding" added by the large font*/}
+      <h1 className="text-title text-teal-300 text-left -indent-1 font-bold">{featured.name}</h1>
       <div>
-        <button>Reproducir</button>
+        <Button variant="solid">Reproducir</Button>
         <button>Mi Lista</button>
       </div>
     </div>
