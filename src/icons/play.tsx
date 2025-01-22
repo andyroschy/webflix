7f4e4ec83ebd5d9ce60ed6b5268e536faff720ce
Andres Rodriguez Schiller <andyroschy@hotmail.com>
2025-01-22 00:08:39 -0300

    added icons to buttons

---
+++ src/icons/play.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from "react";
+
+export function IconPlay(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className="align-middle inline-block"
+      width="14"
+      height="16"
+      viewBox="0 0 14 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.9423 8.2363L2.625 1.875V14.125L11.9423 8.2363Z"
+        stroke="white"
+      />
+    </svg>
+  );
+}
